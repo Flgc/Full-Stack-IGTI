16455d4babdfd3d7e78d005f842091f2590fb672
Fabio Luis <ml@fabiotecnico1.biz>
2020-11-22 10:08:23 -0300

    Bild da da aplicação com erro

--- AppAngular/Tarefas/src/app/app.component.ts
+++ AppAngular/Tarefas/src/app/app.component.ts
@@ -3,7 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  //styleUrls: ['./app.component.scss']
+  styles: [
+    `
+      p {
+        font-weight: bold;
+      }
+      div {
+        color: gray;
+      }
+    `,
+  ],
 })
 export class AppComponent {
   title = 'Tarefas';
