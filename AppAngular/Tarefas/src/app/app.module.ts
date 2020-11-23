e598899a2398afa77423db6d3d51a8856e525d18
Fabio Luis <ml@fabiotecnico1.biz>
2020-11-23 16:05:33 -0300

    Implementando Interpolation, Propety e Event Binding

--- AppAngular/Tarefas/src/app/app.module.ts
+++ AppAngular/Tarefas/src/app/app.module.ts
@@ -5,18 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    AboutComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HomeComponent, AboutComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- AppAngular/Tarefas/src/app/home/home.component.ts
+++ AppAngular/Tarefas/src/app/home/home.component.ts
@@ -3,13 +3,24 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  itemCount = 4;
+  btnText = 'Adicione uma tarefa';
+  objetivoText = 'Objetivos de vida';
+  objetivos: string[] = []; //Corrigido para evitar incompatibilidade de tipo
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  //Inicializa a interface
+  ngOnInit() {
+    this.itemCount = this.objetivos.length;
   }
 
+  addItem() {
+    this.objetivos.push(this.objetivoText);
+    this.objetivoText = ''; //Limpa o campo após adição antes de efetuar a contagem
+    this.itemCount = this.objetivos.length;
+  }
 }
